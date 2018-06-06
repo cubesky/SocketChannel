096210170c3392594044ae201560593024725d37
CubeSky <max_301@live.com>
2018-06-06 10:41:03 +0800

    Feat: DefaultCallback / 5.0

---
+++ src/main/java/party/liyin/socketchannel/DefaultSCCallback.java
@@ -0,0 +1,29 @@
+package party.liyin.socketchannel;
+
+import java.net.Socket;
+
+public class DefaultSCCallback {
+    public class DefaultSCTCPCallback implements TCPSocket.SCTCPCallback {
+        @Override
+        public void onDataArrived(long id, byte[] obj) {
+
+        }
+
+        @Override
+        public void onManagedConnectState(long id, TCPSocket.ConnectState connectState) {
+
+        }
+
+        @Override
+        public void onUnmangedCreated(long id, Socket socket) {
+
+        }
+    }
+
+    public class DefaultSCUDPCallback implements UDPSocket.SCUDPCallback {
+        @Override
+        public void onDataArrived(String ip, int port, byte[] obj) {
+
+        }
+    }
+}
