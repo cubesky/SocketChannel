8313c3795136c4ce0a839422234faac703068701
CubeSky <max_301@live.com>
2018-06-08 12:18:19 +0800

    Feat: DefaultCallback & DefaultServer / 6.0

--- src/main/java/party/liyin/socketchannel/DefaultSCCallback.java
+++ src/main/java/party/liyin/socketchannel/DefaultSCCallback.java
@@ -1,5 +1,7 @@
 package party.liyin.socketchannel;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 
 public class DefaultSCCallback {
@@ -25,5 +27,65 @@ public class DefaultSCCallback {
         public void onDataArrived(String ip, int port, byte[] obj) {
 
         }
+    }
+
+    public static class EchoSCTCPCallback extends DefaultSCTCPCallback {
+        TCPServer tcpServer;
+
+        public EchoSCTCPCallback(TCPServer tcpServer) {
+            this.tcpServer = tcpServer;
+        }
+
+        @Override
+        public void onDataArrived(long id, byte[] obj) {
+            try {
+                tcpServer.sendMessage(id, obj);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class EchoSCUDPServer extends DefaultSCUDPCallback {
+        UDPSocket udpSocket;
+
+        public EchoSCUDPServer(UDPSocket udpSocket) {
+            this.udpSocket = udpSocket;
+        }
+
+        @Override
+        public void onDataArrived(String ip, int port, byte[] obj) {
+            try {
+                udpSocket.sendMessage(ip, port, obj);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public abstract static class StringSCTCPCallback extends DefaultSCTCPCallback {
+        @Override
+        public void onDataArrived(long id, byte[] obj) {
+            try {
+                onDataArrived(id, new String(obj, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public abstract void onDataArrived(long id, String obj);
+    }
+
+    public abstract static class StringSCUDPCallback extends DefaultSCUDPCallback {
+        @Override
+        public void onDataArrived(String ip, int port, byte[] obj) {
+            try {
+                onDataArrived(ip, port, new String(obj, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public abstract void onDataArrived(String ip, int port, String obj);
     }
 }
---
+++ src/main/java/party/liyin/socketchannel/DefaultServer.java
@@ -0,0 +1,44 @@
+package party.liyin.socketchannel;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class DefaultServer {
+    public static class DefaultTCPServer extends TCPServer {
+
+        public DefaultTCPServer(InetSocketAddress address, TCPSocket.SCTCPCallback scTCPCallback) throws IOException {
+            super(address, scTCPCallback);
+        }
+
+        public DefaultTCPServer(InetSocketAddress address, TCPSocket.SCTCPCallback scTCPCallback, boolean fullyManagement) throws IOException {
+            super(address, scTCPCallback, fullyManagement);
+        }
+
+        public void sendBoardcast(String obj) throws IOException {
+            super.sendBroadcast(obj.getBytes());
+        }
+
+        public void sendMessage(long id, String obj) throws IOException {
+            super.sendMessage(id, obj.getBytes());
+        }
+
+    }
+
+    public static class DefaultUDPSocket extends UDPSocket {
+
+        /**
+         * Create an UDP Channel
+         *
+         * @param port          UDP Listen Port
+         * @param scUDPCallback UDP Callback
+         * @throws IOException
+         */
+        public DefaultUDPSocket(int port, SCUDPCallback scUDPCallback) throws IOException {
+            super(port, scUDPCallback);
+        }
+
+        public void sendMessage(String ip, int port, String obj) throws IOException {
+            super.sendMessage(ip, port, obj.getBytes());
+        }
+    }
+}
